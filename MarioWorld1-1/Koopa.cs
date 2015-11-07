cb92681868efed8740a2973d5326f2eeb754a208
unknown <mszauer@mail.usf.edu>
2015-11-06 21:58:04 -0600

    koopa animation code not working properly

--- MarioWorld1-1/Koopa.cs
+++ MarioWorld1-1/Koopa.cs
@@ -21,7 +21,24 @@ namespace MarioWorld1_1 {
             AddSprite("Dead2", new Rectangle(75, 110, 16, 16));
             SetSprite("Walk");
             moveUpDown = movingUpDown;
-            //Rect.Height -= 6;
+        }
+        public override void Render(PointF offsetPosition) {
+            Rectangle visual = SpriteSources[CurrentSprite][CurrentFrame];
+            visual.X = (int)Position.X - ((int)offsetPosition.X - 1);
+            visual.Y = (int)Position.Y - 1;
+            //GraphicsManager.Instance.DrawRect(visual, Color.Red);
+            base.Render(offsetPosition);
+
+
+            Rectangle collision = Rect;
+            //Construct collision from corners
+            collision.X = (int)Corners[CORNER_TOP_LEFT].X;
+            collision.Y = (int)Corners[CORNER_TOP_LEFT].Y;
+            collision.Width = (int)(Corners[CORNER_BOTTOM_RIGHT].X - Corners[CORNER_BOTTOM_LEFT].X);
+            collision.Height = (int)(Corners[CORNER_BOTTOM_RIGHT].Y - Corners[CORNER_TOP_LEFT].Y);
+            //Move into screen space
+            collision.X -= (int)offsetPosition.X;
+            //GraphicsManager.Instance.DrawRect(collision, Color.Blue);
         }
     }
 }
