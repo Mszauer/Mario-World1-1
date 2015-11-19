f00408eea947d5152149ac797cc33a97f603d203
unknown <mszauer@mail.usf.edu>
2015-11-19 17:50:15 -0500

    added state machin to the game, start and play

--- MarioWorld1-1/Game.cs
+++ MarioWorld1-1/Game.cs
@@ -8,6 +8,8 @@ using System.Drawing;
 
 namespace MarioWorld1_1 {
     class Game {
+        public enum State { Start, Play }
+        public State CurrentState = State.Start;
         public static readonly int TILE_SIZE = 16;
         public static Map currentMap = null;
         PointF offsetPosition = new PointF();
@@ -46,24 +48,36 @@ namespace MarioWorld1_1 {
         }
         public void Update(float dt) {
             //currentMap = currentMap.ResolveDoors(hero);
+            if (CurrentState == State.Start) {
 
-            //if !GameOver
-            currentMap.Update(dt, hero);
-            hero.Update(1/30.0f);
-            //else
+            }
+            else if (CurrentState == State.Play) {
+                currentMap.Update(dt, hero);
+                hero.Update(1 / 30.0f);
+            }
 
         }
         public void Render() {
-            PointF offsetPosition = new PointF();
-            offsetPosition.X = hero.Position.X - (float)(7.625 * TILE_SIZE);//7.625 == half of map displayed
-            if (hero.Position.X < 7.625 * TILE_SIZE) {
-                offsetPosition.X = 0;
+            if (CurrentState == State.Start) {
+                Size windowSize = new Size(Program.Window.Size.Width,Program.Window.Height);
+                GraphicsManager.Instance.DrawRect(new Rectangle(new Point(0, 0), windowSize),Color.Black);
+                GraphicsManager.Instance.DrawString("Press Space to Begin!", new Point(windowSize.Width / 2 - 100,windowSize.Height/2), Color.White);
+                if (InputManager.Instance.KeyPressed(OpenTK.Input.Key.Space)) {
+                    CurrentState = State.Play;
+                }
             }
-            if (hero.Position.X > (currentMap[0].Length - 7.625) * TILE_SIZE) {
-                offsetPosition.X = (currentMap.Length - 14) * TILE_SIZE;
+            else if (CurrentState == State.Play) {
+                PointF offsetPosition = new PointF();
+                offsetPosition.X = hero.Position.X - (float)(7.625 * TILE_SIZE);//7.625 == half of map displayed
+                if (hero.Position.X < 7.625 * TILE_SIZE) {
+                    offsetPosition.X = 0;
+                }
+                if (hero.Position.X > (currentMap[0].Length - 7.625) * TILE_SIZE) {
+                    offsetPosition.X = (currentMap.Length - 14) * TILE_SIZE;
+                }
+                currentMap.Render(offsetPosition,hero.Center,hero);
+                hero.Render(new PointF(offsetPosition.X,offsetPosition.Y-1));
             }
-            currentMap.Render(offsetPosition,hero.Center,hero);
-            hero.Render(new PointF(offsetPosition.X,offsetPosition.Y-1));
         }
         public void Shutdown() {
             currentMap.Destroy();
